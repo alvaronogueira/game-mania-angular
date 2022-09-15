9b3faee43a0ec92a97aa446d3bc17a0e5da0c89e
alvaronogueira <alvaronogueira29@gmail.com>
2022-09-15 12:42:54 -0300

    update

--- ang-game/src/app/app.module.ts
+++ ang-game/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './views/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { HeaderComponent } from './views/header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    NgbModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
