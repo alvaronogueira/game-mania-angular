31ea44de82dabfec3840427f3485bac8bc50bbd8
alvaronogueira <alvaronogueira29@gmail.com>
2022-11-02 20:53:56 -0300

    update noticas.service.ts

--- ang-game/src/app/services/noticias.service.ts
+++ ang-game/src/app/services/noticias.service.ts
@@ -6,6 +6,12 @@ import { Observable } from 'rxjs'
   providedIn: 'root'
 })
 export class NoticiasService {
+  url = "http://localhost:3000/noticias"
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getNoticias(): Observable<Noticia[]>{
+    return this.httpClient.get<Noticia[]>(this.url)
+  }
+
 }
