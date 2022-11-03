d6432fb6c2b024ee6a0ac49ecb49d18a8749d59a
alvaronogueira <alvaronogueira29@gmail.com>
2022-11-02 21:07:23 -0300

    update ngFor

--- ang-game/src/app/views/home/home.component.ts
+++ ang-game/src/app/views/home/home.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Noticia } from 'src/app/models/noticia';
+import { NoticiasService } from 'src/app/services/noticias.service';
 
 
 @Component({
@@ -9,12 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private noticiaService: NoticiasService) { }
 
+  listNoticias = [] as Noticia[]
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    this.carregarNoticias()
+  }
 
-
+  carregarNoticias(){
+    this.noticiaService.getNoticias().subscribe( (noticiasRecebidas: Noticia[]) => {
+      this.listNoticias = noticiasRecebidas;
+      console.log(this.listNoticias);
+    } )
+  }
 
 
 }
