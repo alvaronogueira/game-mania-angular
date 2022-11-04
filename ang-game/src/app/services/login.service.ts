a45bc58dab9c6f2a63dc994f544569387ff03719
alvaronogueira <alvaronogueira29@gmail.com>
2022-11-03 21:10:02 -0300

    update login

--- ang-game/src/app/services/login.service.ts
+++ ang-game/src/app/services/login.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  url = "http://localhost:3000/login"
+
+
 }
